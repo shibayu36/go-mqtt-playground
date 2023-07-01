a1cec1024cfd1ac0258c5560be7bfedeb4eecb0d
shibayu36 <shibayu36@gmail.com>
2023-07-01 22:09:20 +0900

    To be able to respond to SUBSCRIBE for the time being

--- broker/main.go
+++ broker/main.go
@@ -45,6 +45,8 @@ func handleConnection(conn net.Conn) {
 		switch packetType >> 4 {
 		case 1:
 			handleConnect(reader, writer)
+		case 8:
+			handleSubscribe(reader, writer)
 		}
 	}
 }
@@ -89,6 +91,61 @@ func handleConnect(reader *bufio.Reader, writer *bufio.Writer) {
 	log.Println("Sent CONNACK packet")
 }
 
+// handleSubscribe handles the SUBSCRIBE packet
+func handleSubscribe(reader *bufio.Reader, writer *bufio.Writer) {
+	// Read the remaining length
+	remainingLength, err := readRemainingLength(reader)
+	if err != nil {
+		log.Println("Error reading remaining length:", err)
+		return
+	}
+	log.Println("Remaining Length:", remainingLength)
+
+	// Read the bytes specified by the Remaining Length
+	payload := make([]byte, remainingLength)
+	_, err = reader.Read(payload)
+	if err != nil {
+		log.Println("Error reading payload:", err)
+		return
+	}
+	log.Printf("Payload: %v\n", payload)
+
+	// Extract the packet ID from the payload
+	packetID := payload[0:2]
+
+	// TODO: Parse other bytes in the payload, and sends appropriate messages to the client.
+
+	// TODO: Extract QoS levels from the payload
+
+	// Define the return codes for the subscription (for this example, assuming success for one subscription)
+	returnCodes := []byte{0x00}
+
+	// Send the SUBACK
+	sendSubAck(writer, packetID, returnCodes)
+}
+
+func sendSubAck(writer *bufio.Writer, packetID []byte, returnCodes []byte) {
+	// Packet Type for SUBACK is 1001 0000 (0x90)
+	packetType := byte(0x90)
+
+	// Remaining Length = length of variable header + length of payload
+	remainingLength := len(packetID) + len(returnCodes)
+	remainingLengthBytes := encodeRemainingLength(remainingLength)
+
+	// Write Fixed Header
+	writer.WriteByte(packetType)
+	writer.Write(remainingLengthBytes)
+
+	// Write Variable Header
+	writer.Write(packetID)
+
+	// Write Payload
+	writer.Write(returnCodes)
+
+	// Flush the writer to ensure all data is sent
+	writer.Flush()
+}
+
 // MQTTのFixed HeaderのRemaining Lengthを読み込む
 func readRemainingLength(reader *bufio.Reader) (int, error) {
 	var value int
@@ -110,4 +167,25 @@ func readRemainingLength(reader *bufio.Reader) (int, error) {
 	}
 
 	return value, nil
+}
+
+func encodeRemainingLength(length int) []byte {
+	encoded := make([]byte, 0)
+
+	for {
+		digit := length % 128
+		length = length / 128
+
+		if length > 0 {
+			digit = digit | 0x80
+		}
+
+		encoded = append(encoded, byte(digit))
+
+		if length == 0 {
+			break
+		}
+	}
+
+	return encoded
 }
