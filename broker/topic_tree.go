2881d94cf11dfe3e4689f688141104bb9611a70f
shibayu36 <shibayu36@gmail.com>
2023-07-02 14:17:40 +0900

    Subscribe and Publish

--- broker/topic_tree.go
+++ broker/topic_tree.go
@@ -1,13 +1,16 @@
 package main
 
+import "strings"
+
 type TopicTreeNode struct {
 	part     string
 	clients  map[*Client]bool
 	subnodes map[string]*TopicTreeNode
 }
 
-func NewTopicTreeNode() *TopicTreeNode {
+func NewTopicTreeNode(part string) *TopicTreeNode {
 	return &TopicTreeNode{
+		part:     part,
 		clients:  make(map[*Client]bool),
 		subnodes: make(map[string]*TopicTreeNode),
 	}
@@ -15,4 +18,48 @@ func NewTopicTreeNode() *TopicTreeNode {
 
 func (n *TopicTreeNode) IsWildcard() bool {
 	return n.part == "#"
+}
+
+func (n *TopicTreeNode) Subscribe(topic string, client *Client) {
+	parts := strings.Split(topic, "/")
+
+	current := n
+	for _, part := range parts {
+		if _, exists := current.subnodes[part]; !exists {
+			current.subnodes[part] = NewTopicTreeNode(part)
+		}
+		current = current.subnodes[part]
+	}
+	current.clients[client] = true
+}
+
+func (n *TopicTreeNode) ClientsToPublish(topic string) []*Client {
+	parts := strings.Split(topic, "/")
+
+	matchingClients := make([]*Client, 0)
+
+	var traverse func(*TopicTreeNode, []string)
+	traverse = func(node *TopicTreeNode, parts []string) {
+		if len(parts) == 0 || node.IsWildcard() {
+			for client := range node.clients {
+				matchingClients = append(matchingClients, client)
+			}
+		}
+
+		if len(parts) > 0 {
+			part := parts[0]
+			if nextNode, exists := node.subnodes[part]; exists {
+				traverse(nextNode, parts[1:])
+			}
+			if nextNode, exists := node.subnodes["+"]; exists {
+				traverse(nextNode, parts[1:])
+			}
+			if nextNode, exists := node.subnodes["#"]; exists {
+				traverse(nextNode, parts)
+			}
+		}
+	}
+	traverse(n, parts)
+
+	return matchingClients
 }
--- broker/topic_tree_test.go
+++ broker/topic_tree_test.go
@@ -22,3 +22,57 @@ func TestTopicTreeNodeIsWildcard(t *testing.T) {
 		assert.False(t, node.IsWildcard())
 	})
 }
+
+func TestTopicTreeSubscribeAndClientsToPublish(t *testing.T) {
+	t.Run("empty topic tree", func(t *testing.T) {
+		node := NewTopicTreeNode("")
+		assert.Equal(t, node.ClientsToPublish(("foo")), []*Client{})
+		assert.Equal(t, node.ClientsToPublish(("foo/bar")), []*Client{})
+		assert.Equal(t, node.ClientsToPublish(("foo/bar/#")), []*Client{})
+		assert.Equal(t, node.ClientsToPublish(("#")), []*Client{})
+		assert.Equal(t, node.ClientsToPublish(("foo/+/baz")), []*Client{})
+	})
+
+	t.Run("simple topic tree", func(t *testing.T) {
+		node := NewTopicTreeNode("")
+		client1 := &Client{ID: "client1"}
+		client2 := &Client{ID: "client2"}
+		client3 := &Client{ID: "client3"}
+
+		node.Subscribe("foo/bar", client1)
+		node.Subscribe("foo/bar/baz", client1)
+
+		node.Subscribe("foo/bar", client2)
+		node.Subscribe("hoge", client2)
+
+		node.Subscribe("foo/bar", client3)
+		node.Subscribe("hoge", client3)
+		node.Subscribe("hoge/fuga", client3)
+
+		assert.ElementsMatch(t, node.ClientsToPublish(("foo/bar")), []*Client{client1, client2, client3})
+		assert.ElementsMatch(t, node.ClientsToPublish(("foo/bar/baz")), []*Client{client1})
+		assert.ElementsMatch(t, node.ClientsToPublish(("hoge")), []*Client{client2, client3})
+		assert.ElementsMatch(t, node.ClientsToPublish(("hoge/fuga")), []*Client{client3})
+		assert.ElementsMatch(t, node.ClientsToPublish(("notexists/1")), []*Client{})
+	})
+
+	t.Run("wildcard topic tree", func(t *testing.T) {
+		node := NewTopicTreeNode("")
+		client1 := &Client{ID: "client1"}
+		client2 := &Client{ID: "client2"}
+		client3 := &Client{ID: "client3"}
+		client4 := &Client{ID: "client4"}
+
+		node.Subscribe("#", client1)
+		node.Subscribe("a/b/c", client2)
+		node.Subscribe("a/+/c", client3)
+		node.Subscribe("a/#", client4)
+
+		assert.ElementsMatch(t, node.ClientsToPublish(("a")), []*Client{client1})
+		assert.ElementsMatch(t, node.ClientsToPublish(("a/b")), []*Client{client1, client4})
+		assert.ElementsMatch(t, node.ClientsToPublish(("a/b/c")), []*Client{client1, client2, client3, client4})
+		assert.ElementsMatch(t, node.ClientsToPublish(("a/b/c/d")), []*Client{client1, client4})
+
+		assert.ElementsMatch(t, node.ClientsToPublish(("b")), []*Client{client1})
+	})
+}
